da3d71e2c1a0af9fa9c632ce828400c1ec9a1589
Tim B <41713011+TBosak@users.noreply.github.com>
2022-09-27 10:06:35 -0500

    added more params

--- index.js
+++ index.js
@@ -7,11 +7,23 @@ var jsonParser = bodyParser.json()
 const port = 3000;
 
 app.post('/', jsonParser, (req, res) =>{
-    Jimp.read(req.body.image,
+    var data = req.body;
+    Jimp.read(data.image,
     (err, img) => {
         if (err) throw err; 
-        if(req.body.resize){img.resize(req.body.resize[0] ?? Jimp.AUTO, req.body.resize[1] ?? Jimp.AUTO)};
-        if(req.body.greyscale){img.greyscale()}
+        if(data.resize){img.resize(Number(data.resize[0]) ?? Jimp.AUTO, Number(data.resize[1]) ?? Jimp.AUTO)};
+        if(data.autocrop){img.autocrop()};
+        if(data.greyscale){img.greyscale()}
+        if(data.blur){img.blur(Number(data.blur))}
+        if(data.posterize){img.posterize(Number(data.posterize))}
+        if(data.sepia){img.sepia()}
+        if(data.invert){img.invert()}
+        if(data.brightness){img.brightness(Number(data.brightness))}
+        if(data.contrast){img.contrast(Number(data.contrast))}
+        if(data.normalize){img.normalize()}
+        if(data.gaussian){img.gaussian(Number(data.gaussian))}
+        if(data.flip){img.flip(data.flip[0], data.flip[1])}
+        if(data.mirror){img.mirror(data.mirror[0], data.mirror[1])}
         img.getBuffer(img.getMIME(), (err, buffer) => {
             const b64 = Buffer.from(buffer).toString('base64');
             const mimeType = img.getMIME(); // e.g., image/png
