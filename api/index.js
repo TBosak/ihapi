fa9990029b8cf501d4ffa6b17b0e941a7f77d84d
Tim B <41713011+TBosak@users.noreply.github.com>
2023-02-10 21:05:12 -0600

    added get request to api/index.js

--- api/index.js
+++ api/index.js
@@ -4,6 +4,8 @@ const Jimp = require('jimp');
 const app = express();
 const port = 3000;
 
+app.get('/', (req, res) => res.send('Hello ihapi!'));
+
 app.post('/', express.json(), (req, res) =>{
     var data = req.body;
     Jimp.read(data.image,
